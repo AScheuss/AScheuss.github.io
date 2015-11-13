beac19fb76bce664ef1ac7ee04a550287461ab35
AScheuss <andreas.scheuss@gmx.ch>
2015-11-13 14:11:40 +0100

    First trial on d3

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+//TODO add Licences
+
+var addMap = function() {
+
+    var width = 960,
+        height = 500;
+
+    var path = d3.geo.path()
+        .projection(null);
+
+    var svg = d3.select("div s").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    d3.json("data/ch.json", function(error, ch) {
+        svg.append("path")
+            .datum(topojson.feature(ch, ch.objects.country))
+            .attr("class", "country")
+            .attr("d", path);
+
+        svg.append("path")
+            .datum(topojson.mesh(ch, ch.objects.cantons, function(a, b) { return a !== b; }))
+            .attr("class", "canton-boundaries")
+            .attr("d", path);
+    });
+}
